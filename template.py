9cecb1c72e9636bd18a6f2e93f043dc65d65f3d6
Somitra Singh Kushwah <somitrasingh07@gmail.com>
2025-10-23 00:11:03 -0500

    chenged project folder

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name="end-to-end-ds-project"
+project_name="dsproject"
 
 list_of_files=[
     ".github/workflows/.gitkeep",
